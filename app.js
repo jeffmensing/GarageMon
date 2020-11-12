d7cc21f77f6be3ebb3a6c9b922a562b8db43296c
jeffm <jeff.mensing@gmail.com>
2020-11-11 22:01:08 -0600

    Updated index, app.js and main.css

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-//document.getElementById('button3').addEventListener('click', getExternal);
-setInterval(getExternal,10);
-
 // Get from external API
-function getExternal() {
+window.onload = function getExternal() {
   fetch('https://jamgarageapi.azurewebsites.net/api/garages')
     .then(function(res){
       return res.json();
@@ -11,7 +8,12 @@ function getExternal() {
       console.log(data);
       let output = '';
       data.forEach(function(garage) {
-        output += `<div class="garage"><ul class"garagename">${garage.garageName}<li>${garage.numParkingSpotsOccupied} Out of ${garage.maxParkingSpots} Used</li><li>${Math.trunc((garage.numParkingSpotsOccupied/garage.maxParkingSpots)*100)}% Capacity</li></ul></div>`;
+        var style = "style=color:green";
+        var percent = Math.trunc((garage.numParkingSpotsOccupied/garage.maxParkingSpots)*100);
+        if (percent >= 85) {
+          style = "style=color:red";
+        }
+        output += `<div class="garage"><ul class"garagename">${garage.garageName}/ Id ${garage.id}<li>${garage.numParkingSpotsOccupied} Out of ${garage.maxParkingSpots} Used</li><li ${style}>${percent}% Capacity</li></ul></div>`;        
       });
       document.getElementById('output').innerHTML = output;
     })
